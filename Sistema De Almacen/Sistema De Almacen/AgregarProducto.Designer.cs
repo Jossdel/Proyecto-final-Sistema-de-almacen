306498dd25e1a0f1d6996de9b840da084d71ffc8
Jossdel <josuedelgadil01@gmail.com>
2025-07-17 20:22:57 -0400

    eliminacion

--- Sistema De Almacen/Sistema De Almacen/AgregarProducto.Designer.cs
+++ Sistema De Almacen/Sistema De Almacen/AgregarProducto.Designer.cs
@@ -47,8 +47,6 @@ namespace Sistema_De_Almacen
             this.lblPrecio = new System.Windows.Forms.Label();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnAgregarProducto = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.lblEstado = new System.Windows.Forms.Label();
             this.lblCantidad = new System.Windows.Forms.Label();
             this.lblNecesidad = new System.Windows.Forms.Label();
@@ -132,24 +130,6 @@ namespace Sistema_De_Almacen
             this.btnAgregarProducto.Text = "     Agregar Producto";
             this.btnAgregarProducto.UseVisualStyleBackColor = false;
             this.btnAgregarProducto.Click += new System.EventHandler(this.btnAgregarProducto_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(449, 885);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 29);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(457, 893);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 29);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
             // 
             // lblEstado
             // 
@@ -248,8 +228,6 @@ namespace Sistema_De_Almacen
             this.Controls.Add(this.lblNecesidad);
             this.Controls.Add(this.lblCantidad);
             this.Controls.Add(this.lblEstado);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.lblPrecio);
@@ -264,9 +242,6 @@ namespace Sistema_De_Almacen
             this.PerformLayout();
 
         }
-
-        private Label label1;
-        private Label label2;
         private Label lblEstado;
         private Label lblCantidad;
         private Label lblNecesidad;
